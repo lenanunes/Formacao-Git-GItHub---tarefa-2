7d46cffed9b1434907f2d6c54da6ce5d857a6beb
Helena Nunes <180981261+lenanunes@users.noreply.github.com>
2024-09-14 15:52:29 +0100

    Adicionar novo ficheiro exercício 2

---
+++ exercicio2.py
@@ -0,0 +1,22 @@
+
+
+lista = [13,27,16,39,112] 
+print(len(lista))
+lista.append(33)
+lista.sort()
+print(lista)
+#print(sorted(lista))
+
+lista.remove(13)
+print(lista)
+lista.pop(2)
+print(lista)
+
+
+l = [13, 39,112, 20, 27, 68] 
+if 20 in l :
+    print("Valor encontrado na lista")
+else:
+    print("Valor Não encontrado na lista")
+    
+    
